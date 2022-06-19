8f78e4333a29f2354e55635445809ef1a7b03c38
Yiannis Karaziakos <yiannis.karaziakos@icloud.com>
2022-06-18 20:42:00 -0400

    Complete exercise 1.
      - Add loop around my_num to make testing easier.
      - Populate function with one-liner.
      - Output results to out.txt

--- chapter_1/exercise_1_1.cpp
+++ chapter_1/exercise_1_1.cpp
@@ -1,11 +1,14 @@
 #include <cstdio>
 
 int absolute_value(int x) {
-  // Your code here
-  return 0;
+  //Flip sign if negative and return
+  return x > 0 ? x : -1*x;
 }
 
 int main() {
-  int my_num = -10;
-  printf("The absolute value of %d is %d.\n", my_num, absolute_value(my_num));
+
+  for(int my_num=-100; my_num <= 100; my_num++)
+  {
+    printf("The absolute value of %d is %d.\n", my_num, absolute_value(my_num));
+  }
 }
